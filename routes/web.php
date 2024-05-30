89aee49fc97643fc7df8367203376eef19c46916
JK-B0T <JK-B0T@users.noreply.github.com>
2024-05-30 11:24:08 +0200

    Test routes functionality

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/chiperia', function () {
+    return 'Benvinguts a la chiperia de Juan';
+});
+
+Route::get('/chiperia/{dobob}', function ($chipe, $popi = null) {
+    return "El chipe popo fue dobobado ¡Nooooo! D:";
+});
+
+Route::get('/chiperia/{chipe}+{popi?}', function ($chipe, $popi = null) {
+    if ($popi) {
+        return "Chipe Juan: {$chipe} y {$popi}";
+    }
+    return "Chipe Juan: {$chipe}, pero no popi :(";
+});
